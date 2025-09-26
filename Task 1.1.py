c3bbbe88d3c5e51630b87d0b6657539cb8f056ef
Advay Suvarna <advaysuvarna77@gmail.com>
2025-09-26 20:14:04 +0530

    initial commit

---
+++ Task 1.1.py
@@ -0,0 +1,48 @@
+n=int(input("Enter the number"))
+y=n
+Lstrings=[]
+while(n>0): 
+    x=input("Enter the strings")
+    Lstrings.append(x)
+    n-=1
+
+dict={}
+for word in Lstrings:
+    for letter in word:
+        if letter in dict:
+            dict[letter]+=1
+        else:
+            dict[letter]=1
+
+print(Lstrings)
+print(dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
